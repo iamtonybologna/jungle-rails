d091e4a0560a3f69748478eb568fdb78ce6136c2
Anthony Langford <anthony.langford@gmail.com>
2017-02-17 16:34:26 +0000

    added template from orders/show to be generated in email

---
+++ test/mailers/previews/order_mailer_preview.rb
@@ -0,0 +1,10 @@
+class OrderMailerPreview < ActionMailer::Preview
+
+  def order_email
+    @order = Order.second
+    @user = User.find_by(email: @order.email)
+    @line_items = LineItem.where(order_id: @order.id)
+    OrderMailer.order_email(@user, @order, @line_items)
+  end
+
+end
