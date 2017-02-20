83ac54089ce49856bf18e98af6cab8108f8b72a5
Anthony <heyzeus@GALAXY.local>
2017-02-20 18:01:22 -0500

    changed syntax of case sensitive validation option to match older version of rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, uniqueness: true, case_sensitive: false
+  validates :email, presence: true, uniqueness: { :case_sensitive => false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 }
 
