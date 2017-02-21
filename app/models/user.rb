2d4cd334a6beb72b6ab3242439bd17dd1d302c2c
Anthony <heyzeus@GALAXY.local>
2017-02-21 10:47:59 -0500

    added authenticate_with_credentials method to User class and moved logic from sessions controller. tested for edge cases and added support for email form whitespace and case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,14 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true, length: { minimum: 6 }
 
+  def self.authenticate_with_credentials(email, password)
+    # use create method from sessions_controller and pass in params
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
 end
